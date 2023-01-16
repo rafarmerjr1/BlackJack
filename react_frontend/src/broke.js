7fb0df292cb5b5711dbd213d985215c7c08d8c95
Robert Farmer <robert.farmer000@gmail.com>
2023-01-16 17:13:34 -0500

    spelling

--- react_frontend/src/broke.js
+++ react_frontend/src/broke.js
@@ -12,7 +12,7 @@ export function Broke(props) {
             <button onClick={GoHome}> 
              <img className="home-Image" src={require('./images/titlecard.png')} alt="homeImage"/> 
             </button>
-            <h1 className="fade-in-image section" ><span className="outcome_color">Whoa Parter - </span> You're all out of money.</h1>
+            <h1 className="fade-in-image section" ><span className="outcome_color">Whoa Partner - </span> You're all out of money.</h1>
             <h4>Maybe you should try starting a new game.</h4>
         </div>
     )
